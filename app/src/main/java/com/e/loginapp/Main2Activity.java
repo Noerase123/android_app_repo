6b355b7425727f14b5f0d2380051b7bff1db605c
isaac <noerase12@gmail.com>
2020-01-13 10:09:43 +0800

    updates calc

--- app/src/main/java/com/e/loginapp/Main2Activity.java
+++ app/src/main/java/com/e/loginapp/Main2Activity.java
@@ -2,7 +2,10 @@ package com.e.loginapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        LinearLayout calcbtn = (LinearLayout) findViewById(R.id.calcbtn);
+
+        calcbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Main2Activity.this, CalculatorActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }

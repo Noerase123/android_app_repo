8bdad746ef5285312461bacd18047880e6da27bf
Noerase123 <noerase12@gmail.com>
2020-02-04 14:44:15 +0800

    app commit

--- app/src/main/java/com/e/loginapp/Main2Activity.java
+++ app/src/main/java/com/e/loginapp/Main2Activity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -13,6 +14,68 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        LinearLayout paymentbtn = (LinearLayout) findViewById(R.id.paymentbtn);
+        LinearLayout billbtn = (LinearLayout) findViewById(R.id.billbtn);
+        LinearLayout issuebtn = (LinearLayout) findViewById(R.id.issuebtn);
+        LinearLayout profilebtn = (LinearLayout) findViewById(R.id.profilebtn);
+        LinearLayout event1btn = (LinearLayout) findViewById(R.id.event1btn);
+        LinearLayout event2btn = (LinearLayout) findViewById(R.id.event2btn);
+        LinearLayout event3btn = (LinearLayout) findViewById(R.id.event3btn);
+
+        paymentbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Main2Activity.this, paymentActivity.class);
+                startActivity(i);
+            }
+        });
+
+        billbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Main2Activity.this, BillListActivity.class);
+                startActivity(i);
+            }
+        });
+
+        issuebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Main2Activity.this, IssueActivity.class);
+                startActivity(i);
+            }
+        });
+
+        profilebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Main2Activity.this,ProfileActivity.class);
+                startActivity(i);
+            }
+        });
+
+        event1btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Main2Activity.this, "Event # 1", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        event2btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Main2Activity.this, "Event # 2", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        event3btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Main2Activity.this, "Event # 3", Toast.LENGTH_LONG).show();
+            }
+        });
+
 
     }
 }
---
+++ app/src/main/java/com/e/loginapp/paymentActivity.java
@@ -0,0 +1,36 @@
+package com.e.loginapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class paymentActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+
+
+        Spinner dropdown = findViewById(R.id.spinner1);
+        String[] items = new String[]{"Select Invoice to pay","OFF202000591 - (P 5388.35)","OFF202000569 - (P 5388.35)"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
+
+        ImageView captureBtn = (ImageView) findViewById(R.id.captureBtn);
+
+        captureBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(paymentActivity.this, "Open Camera", Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+}

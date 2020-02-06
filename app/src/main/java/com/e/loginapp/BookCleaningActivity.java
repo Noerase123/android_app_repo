d916c53166dd73d08dcb4382dbd84d71047a346e
Noerase123 <noerase12@gmail.com>
2020-02-06 09:51:53 +0800

    updates and

---
+++ app/src/main/java/com/e/loginapp/BookCleaningActivity.java
@@ -0,0 +1,14 @@
+package com.e.loginapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BookCleaningActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_cleaning);
+    }
+}
---
+++ app/src/main/java/com/e/loginapp/LeaseActivity.java
@@ -0,0 +1,14 @@
+package com.e.loginapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LeaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lease);
+    }
+}
